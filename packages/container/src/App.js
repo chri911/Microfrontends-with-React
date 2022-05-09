443e197002fc469d11dbd1c7b7b5da630eace67b
Khrystyna Iezik <Khrystyna.Iezik@tx.local>
2022-05-09 15:19:41 +0300

    add invalidation step

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import { MarketingApp } from './components/MarketingApp';
 export const App = () => {
   return (
     <>
-      <h1>Hi there</h1>
+      <h1>Hi there!</h1>
       <MarketingApp />
     </>
   )
